629d3e5c1267d7dbae1c8ed5197057aae32eb137
Lyzzi Brooks <brookse@msoe.edu>
2016-08-29 16:42:47 -0500

    hopefully delay

--- js/game.js
+++ js/game.js
@@ -180,43 +180,44 @@ function checkWon() {
     // increase level
     currentLevel.level += 1;
     
-    // switch to new level
-    switch (currentLevel.level) {
-      case 2:
-        levelTwo();
-        break;
-      case 3:
-        levelThree();
-        break;
-      case 4:
-        levelFour();
-        break;
-      case 5:
-        levelFive();
-        break;
-      case 6:
-        levelSix();
-        break;
-      case 7:
-        levelSeven();
-        break;
-      case 8:
-        levelEight();
-        break;
-      case 9:
-        levelNine();
-        break;
-      case 10:
-        levelTen();
+    game.time.events.add(Phaser.Timer.SECOND * 2, function() {
+      // switch to new level
+      switch (currentLevel.level) {
+        case 2:
+          levelTwo();
           break;
-      case 11:
-        victory();
-        break;
-      default:
-        console.log('level oops');
-    }
-    
-    won = false;
+        case 3:
+          levelThree();
+          break;
+        case 4:
+          levelFour();
+          break;
+        case 5:
+          levelFive();
+          break;
+        case 6:
+          levelSix();
+          break;
+        case 7:
+          levelSeven();
+          break;
+        case 8:
+          levelEight();
+          break;
+        case 9:
+          levelNine();
+          break;
+        case 10:
+          levelTen();
+          break;
+        case 11:
+          victory();
+          break;
+        default:
+          console.log('level oops');
+      }
+      won = false;
+    }, this);
   }
 }
 
