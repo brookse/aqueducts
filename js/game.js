ae4f7e5cf53ca83f6d4f356d476b1b650be1641b
Lyzzi Brooks <brookse@msoe.edu>
2016-08-29 16:44:31 -0500

    hopefully delay

--- js/game.js
+++ js/game.js
@@ -177,10 +177,10 @@ function checkWon() {
   		}
     }
     
-    // increase level
-    currentLevel.level += 1;
-    
     game.time.events.add(Phaser.Timer.SECOND * 2, function() {
+      // increase level
+      currentLevel.level += 1;
+      
       // switch to new level
       switch (currentLevel.level) {
         case 2:
